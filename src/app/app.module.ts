fb52e9eea1fae5d8ee4a99bd5a6534b90d5610bb
Rodzman <designrsp@gmail.com>
2018-04-14 21:03:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ContentComponent } from './content/content.component';
+import { GeruService } from './geru.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InfoComponent,
+    BreadcrumbComponent,
+    ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GeruService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
