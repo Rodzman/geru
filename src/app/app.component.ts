d49cd66f5c316b6b8946399f283a14900bcd46a1
Rodzman <designrsp@gmail.com>
2018-04-15 19:33:21 -0300

    Beautify Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-
 import { Component, OnInit  } from '@angular/core';
 import * as $ from 'jquery';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Look jQuery Animation working in action!';
+  title = 'Geru App';
 
   public ngOnInit()
   {
