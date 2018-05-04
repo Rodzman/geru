eafc6eba94294411f61211a3216d2894f6fc26ca
Rodzman <designrsp@gmail.com>
2018-05-04 12:14:24 -0300

    Build Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  watch = require('gulp-watch');
+    sass = require('gulp-sass'),
+    cssMin = require('gulp-css');
+    watch = require('gulp-watch');
+    concat = require('gulp-concat');
 
 // task default
-gulp.task('default', ['bootstrap-scss', 'fontawesome', 'app-sass', 'watch']);
+gulp.task('default', ['bootstrap-scss', 'app-sass', 'fontawesome', 'cssMinfy']);
 
 // task to bootstrap scss
 gulp.task('bootstrap-scss', function () {
@@ -23,6 +25,12 @@ gulp.task('app-sass', function () {
 gulp.task('fontawesome', function () {
   return gulp.src('node_modules/@fortawesome/fontawesome-free-webfonts/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/assets/css'));
+});
+
+gulp.task('cssMinfy', function(){
+  return gulp.src('src/assets/**/*.css')
+    .pipe(cssMin())
     .pipe(gulp.dest('src/assets/css'));
 });
 
